6be598bf5304c08830271aecc97928202c78c155
Azia Bay-Asen <aziabayasen@gmail.com>
2025-03-10 16:20:14 -0500

    More popup

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -3,8 +3,17 @@ import { useState, useEffect } from "react";
 import Papa from "papaparse";
 
 export default function Sidebar() {
-	const [tweets, setTweets] = useState([]);
-	const [visibleCount, setVisibleCount] = useState(3); // Start with 3 tweets
+    const numVisiblePosts = 3;
+	const [posts, setPosts] = useState([]);
+
+    const morePopupPostsPerPage = 50;
+    const [showMorePopup, setShowMorePopup] = useState(false);
+    const [morePopupPageIndex, setMorePopupPageIndex] = useState(0);
+
+    function openMorePopup() {
+        setShowMorePopup(true);
+        setMorePopupPageIndex(0);
+    }
 
 	useEffect(() => {
 		fetch("/preprocessed_data_utf8.csv")
@@ -18,18 +27,14 @@ export default function Sidebar() {
 				console.log("Parsed CSV Data:", result.data.slice(0, 10)); // Debugging
 				
 				if (result.data.length > 0 && result.data[0]["text"]) {
-				  setTweets(result.data.map(row => row["text"])); // Store all tweets
+				  setPosts(result.data.map(row => row["text"])); // Store all posts
 				} else {
-				  console.warn("No valid tweets found.");
+				  console.warn("No valid posts found.");
 				}
 			  },
 			});
 		  });
 	  }, []);
-
-	const loadMoreTweets = () => {
-		setVisibleCount((prevCount) => Math.min(prevCount + 5, 8)); // Load 5 more tweets
-	};
     
     return(
         <aside
@@ -56,7 +61,7 @@ export default function Sidebar() {
                         borderLeft: "solid 1px #D4DBE2",
                         borderRight: "solid 1px #D4DBE2",
                         borderTop: "solid 1px #D4DBE2"}}>
-                    {tweets.slice(0, visibleCount).map((tweet, index) => (
+                    {posts.slice(0, numVisiblePosts).map((post, index) => (
                         <div
                             key={index}
                             style={{
@@ -65,13 +70,13 @@ export default function Sidebar() {
                                 minHeight: "80px",
                                 padding: "8px",
                                 width: "100%"}}>
-                            {tweet}
+                            {post}
                         </div>
                     ))}
                 </div>
                 
                 <div>
-                    {visibleCount < 8 && (
+                    {numVisiblePosts < 8 && (
                         <div
                             style={{
                                 borderBottom: "solid 1px black",
@@ -79,7 +84,7 @@ export default function Sidebar() {
                                 display: "inline-block",
                                 fontSize: "16px",
                                 fontWeight: "300"}}
-                            onClick={loadMoreTweets}>
+                            onClick={openMorePopup}>
                             More
                         </div>
                     )}
@@ -108,6 +113,101 @@ export default function Sidebar() {
                             textAlign: "center"}}>
                         Line graph here
                     </div>
+            </div>
+
+            <div
+                style={{
+                    background: "rgba(0, 0, 0, 0.5)",
+                    display: showMorePopup ? "inherit" : "none",
+                    height: "100%",
+                    left: "0px",
+                    padding: "50px",
+                    position: "fixed",
+                    top: "0px",
+                    width: "100%",
+                    zIndex: "1001"}}>
+                <div
+                    style={{
+                        background: "white",
+                        borderRadius: "8px",
+                        boxShadow: "2px 2px 10px rgba(0, 0, 0, 0.3)",
+                        display: "flex",
+                        flexDirection: "column",
+                        height: "100%",
+                        width: "100%"}}>
+                    <button
+                        style={{
+                            color: "white",
+                            cursor: "pointer",
+                            backgroundColor: "red",
+                            border: "none",
+                            borderRadius: "50%",
+                            fontSize: "14px",
+                            fontWeight: "bold",
+                            height: "20px",
+                            marginLeft: "calc(100% - 20px - 5px)",
+                            marginTop: "5px",
+                            width: "20px"}}
+                        onClick={() => setShowMorePopup(false)}>
+                        X
+                    </button>
+
+                    <div style={{
+                        fontSize: "16px",
+                        marginLeft: "15px",
+                        marginRight: "15px"}}>
+                        {(morePopupPageIndex * morePopupPostsPerPage + 1).toLocaleString()} - {Math.min(posts.length, ((morePopupPageIndex + 1) * morePopupPostsPerPage)).toLocaleString()} of {posts.length.toLocaleString()} posts
+                    </div>
+
+                    <div style={{
+                        columnGap: "8px",
+                        display: "flex",
+                        flexDirection: "row",
+                        fontSize: "16px",
+                        marginLeft: "15px",
+                        marginRight: "15px"}}>
+                        <div
+                            style={{
+                                borderBottom: "solid 1px black",
+                                cursor: "pointer",
+                                display: "inline-block",
+                                fontSize: "16px",
+                                fontWeight: "300"}}
+                            onClick={() => setMorePopupPageIndex(Math.max(0, morePopupPageIndex - 1))}>
+                            Prev
+                        </div>
+
+                        <div
+                            style={{
+                                borderBottom: "solid 1px black",
+                                cursor: "pointer",
+                                display: "inline-block",
+                                fontSize: "16px",
+                                fontWeight: "300"}}
+                            onClick={() => setMorePopupPageIndex(Math.min(Math.floor((posts.length - 1) / morePopupPostsPerPage), morePopupPageIndex + 1))}>
+                            Next
+                        </div>
+                    </div>
+
+                    <div
+                        style={{
+                            borderTop: "solid 1px #D4DBE2",
+                            marginTop: "15px",
+                            maxHeight: "calc(100% - 100px)",
+                            overflowY: "scroll"}}>
+                        {posts.slice(morePopupPageIndex * morePopupPostsPerPage, morePopupPageIndex * morePopupPostsPerPage + morePopupPostsPerPage).map((post, index) => (
+                            <div
+                                key={index}
+                                style={{
+                                    borderBottom: "solid 1px #D4DBE2",
+                                    fontSize: "16px",
+                                    padding: "8px 15px",
+                                    width: "100%"}}>
+                                {(morePopupPageIndex * morePopupPostsPerPage + index + 1).toLocaleString()}. {post}
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </aside>
     );
