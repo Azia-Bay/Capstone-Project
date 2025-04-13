399fb2182e2cbe9ae1a6808bd8de70c7a171378b
vikranthchinnam <vikranth.chinnam@gmail.com>
2025-04-13 17:52:23 -0500

    /datetime api and make backend infinitely faster by making it multi process

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,24 +1,17 @@
 from fastapi import FastAPI
 from bluesky import RealTimeData
 import uvicorn
-import spacy
-from geopy.geocoders import Nominatim
-from shapely.geometry import Point
-import geopandas as gpd
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 import time
 import mysql.connector
 from mysql.connector import errorcode
-
+import subprocess
 import json
 from asyncio import sleep
-import os
-import re
 from predictor import DisasterPredictor, LSTMClassifier
-from asyncio import sleep
-import json
-from transformers import pipeline
+from multiprocessing import Process
+from datetime import datetime, timezone, timedelta
 # use Celery for cron job in the future.
 app = FastAPI()
 
@@ -98,104 +91,6 @@ for table_name in TABLES:
 def read_root():
     return {"Hello": "World"}
 
-# Load shapefile for US states
-us_map = gpd.read_file("/app/src/shapely/ne_110m_admin_1_states_provinces.shp", encoding="utf-8")
-# Only map US
-us_map = us_map[us_map["admin"] == "United States of America"]
-
-#US states to detect
-state_abbreviations = {
-"AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", "CA": "California",
-"CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "FL": "Florida", "GA": "Georgia",
-"HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa",
-"KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MD": "Maryland",
-"MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi",
-"MO": "Missouri", "MT": "Montana", "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire",
-"NJ": "New Jersey", "NM": "New Mexico", "NY": "New York", "NC": "North Carolina",
-"ND": "North Dakota", "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PA": "Pennsylvania",
-"RI": "Rhode Island", "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee",
-"TX": "Texas", "UT": "Utah", "VT": "Vermont", "VA": "Virginia", "WA": "Washington",
-"WV": "West Virginia", "WI": "Wisconsin", "WY": "Wyoming"
-}
-
-us_states = set(state_abbreviations.values())
-
-def extract_hashtags(text):
-    # Extracts potential location names from hashtags.
-    return [tag[1:] for tag in re.findall(r"#\w+", text)]
-
-# takes in a argument of text and returns an Optional max_gpe
-def extract_locations(text):
-    nlp = spacy.load('en_core_web_trf')
-
-    #text = text.upper()
-    doc = nlp(text)
-    locations = {}
-    for item in doc.ents:
-        if item.label_ == 'GPE':
-            # Convert state abbreviations
-            loc_name = state_abbreviations.get(item.text, item.text) 
-            locations[loc_name] = locations.get(loc_name, 0) + 1
-    # Check hashtags for locations
-    hashtags = extract_hashtags(text)
-    for tag in hashtags:
-        tag_doc = nlp(tag)
-        for entity in tag_doc.ents:
-            if entity.label_ == "GPE":
-                loc_name = state_abbreviations.get(entity.text, entity.text)
-                locations[loc_name] = locations.get(loc_name, 0) + 1
-
-    return locations if locations else None
-    
-def classify_relevant_state(text, locations):
-    if not locations:
-        return None
-
-    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-
-    # Run zero-shot classification with locations as candidate labels
-    result = classifier(text, candidate_labels=list(locations.keys()))
-    return result["labels"][0]  # Return the highest-ranked location
-    
-def locate_disaster(text, locations):
-    if not locations:
-        return None, None
-    
-    #Get the most mentioned location
-    if len(locations) > 1:
-        most_mentioned = classify_relevant_state(text, locations)
-    else:
-        most_mentioned = max(locations, key=locations.get)
-
-    #If it's a state, return
-    if most_mentioned in us_states:
-        return most_mentioned, most_mentioned 
-    
-    #Get state if we only have a city name using Geopy
-    geolocator = Nominatim(user_agent="GetLoc")
-    location = geolocator.geocode(most_mentioned)
-    if location:
-        address = location.address.split(", ")
-        for part in address:
-            if part in us_states:
-                return most_mentioned, part
-
-    return most_mentioned, None
-    
-# Gives (latitude, longitude) from argument location <- result of locate(text)
-def get_coordinates(location):
-    loc = Nominatim(user_agent="GetLoc")
-    getLoc = loc.geocode(location)
-    if getLoc:
-        if is_within_us(getLoc.latitude, getLoc.longitude):
-            return getLoc.latitude, getLoc.longitude
-    return None, None
-
-def is_within_us(latitude, longitude):
-    # Check if the coordinates are within the U.S. boundaries.
-    point = Point(longitude, latitude)
-    return us_map.geometry.contains(point).any()
-
 @app.get("/all-data")
 def get_all_data():
     cursor.execute("SELECT * FROM disaster_data")
@@ -216,7 +111,28 @@ def get_all_data():
         data.append(temp)
     return data
 
-# @app.get("/datatime")
+# called from the frontend
+# Example URL to CALL: http://localhost:8000/datetime?start="2025-03-31T15:15:00"&end="2025-03-31T15:16:00" 
+@app.get("/datetime")
+def get_data_time(start:str, end:str):
+    # start and end given in the following format "yyyy-mm-ddThr:min:sec"
+    query = f"SELECT * FROM disaster_data WHERE timestamp BETWEEN {start} AND {end}"
+    cursor.execute(query)
+
+    res = cursor.fetchall()
+    data = []
+    for row in res:
+        temp = {}
+        temp["tweet_id"] = row[0]
+        temp["tweet"] = row[1]
+        temp["model"] = row[2]
+        temp["state"] = row[3]
+        temp["city"] = row[4]
+        temp["latitude"] = row[5]
+        temp["longitude"] = row[6]
+        temp["timestamp"] = row[7]
+        data.append(temp)
+    return data
 
 @app.get("/nondisaster-data")
 def get_nondisaster_data():
@@ -232,95 +148,49 @@ def get_nondisaster_data():
         data.append(temp)
     return data
 
-def load_model(model_path):
-    model = torch.load(model_path).to(device)
-    model.eval() # set the model to evaluation model
-    return model
 
-def tokenize(text):
-    max_length = self.config['max_length']
-    words = text.split()
-    tokens = [self.word_to_idx.get(word, self.word_to_idx['<UNK>']) for word in words]
-    
-    # Pad or truncate
-    if len(tokens) < max_length:
-        tokens = tokens + [self.word_to_idx['<PAD>']] * (max_length - len(tokens))
-    else:
-        tokens = tokens[:max_length]
-    
-    return tokens
-
-model = DisasterPredictor(model_dir="/app/src/disaster_model")
-real_time = RealTimeData()
 async def data_generator():
-    print("I get hit")
     while(True):
-        # get real time tweets
-        data = real_time.get_all()
-        print(data)
-        disaster_query = "" # INSERT INTO tbl_name (a,b,c) VALUES
-        non_disaster_query = "" 
-        return_data = []
-        for tweet in data:
-            # Call Spacy Function to get location.
-            print(tweet)
-            result = model.predict(tweet)
-            disaster = result['predicted_class']
-            tweet = tweet.replace("'", "\\'")
-            if disaster == 0:
-                non_disaster_query += f"('{tweet}'), "
-                continue
-            city, state = locate_disaster(tweet, extract_locations(tweet))
-            await sleep(1)
-            print(f"I get here {city} {state}")
-            # if city is None:
-            #     continue
-            coordinates = get_coordinates(location=city)
-            latitude, longitude = coordinates[0], coordinates[1]
-            if city == state:
-                city = None
-            if latitude is None or longitude is None:
-                continue
-            if city:
-                city = city.replace("\\", "")
-            if state:
-                state = state.replace("\\", "")
-            # Call geopy library to get latitude and longitude
-            # Call Model to classify the tweet
-            if city:
-                city = city.replace("\\", "")
-            if state:
-                state = state.replace("\\", "")
-            print(tweet, latitude, longitude, city, state, disaster)
-            disaster_query += f"('{tweet}', {disaster}, '{state}', '{city}', '{latitude}', '{longitude}'), "
-            return_data.append({"tweet":tweet, "disaster":disaster, "state":state, "city":city, "latitude":latitude, "longitude":longitude})
-            # append Value to query
-        # make an insert call to database
-        if disaster_query != "":
-            disaster_query = disaster_query[:len(disaster_query)-2]
-            disaster_query += ";"
-            disaster_query = "INSERT INTO `disaster_data` (tweet, model, state, city, latitude, longitude) VALUES " + disaster_query
-            print(disaster_query)
-            cursor.execute(disaster_query)
-
-        if non_disaster_query != "":
-            non_disaster_query = non_disaster_query[:len(non_disaster_query)-2]
-            non_disaster_query += ";"
-            non_disaster_query = "INSERT INTO `non_disaster_data` (tweet) VALUES " + non_disaster_query
-            print(non_disaster_query)
-            cursor.execute(non_disaster_query)
-        cnx.commit()
-        json_data = json.dumps(return_data)
+        tzinfo = timezone(timedelta(hours=-5))
+        start = (datetime.now() - timedelta(hours=0, minutes=1, seconds=1)).strftime('%Y-%m-%dT%H:%M:%S')
+        end = datetime.now(tzinfo).strftime('%Y-%m-%dT%H:%M:%S')
+        query = f"SELECT * FROM disaster_data WHERE timestamp BETWEEN \"{start}\" AND \"{end}\""
+        cursor.execute(query)
+        res = cursor.fetchall()
+    
+        data = []
+        for row in res:
+            temp = {}
+            temp["tweet_id"] = row[0]
+            temp["tweet"] = row[1]
+            temp["model"] = row[2]
+            temp["state"] = row[3]
+            temp["city"] = row[4]
+            temp["latitude"] = row[5]
+            temp["longitude"] = row[6]
+            temp["timestamp"] = row[7]
+            data.append(temp)
+        json_data = json.dumps(data)
         yield f"event: newTweets\ndata: {json_data}\n\n"
         await sleep(60)
+        
+        
 
 @app.get("/stream")
 async def stream():
     return StreamingResponse(data_generator(), media_type="text/event-stream")
 
-@app.get('/real-time-data')
-def get_real_time_data():
-    pass
+def start_data_processing():
+    subprocess.run(["python", "./src/data_processing.py"])
+
+def start_server():
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
+    data_process = Process(target=start_data_processing)
+    server_process = Process(target=start_server)
+    data_process.start()
+    server_process.start()
+
+    server_process.join()
+    data_process.join()
